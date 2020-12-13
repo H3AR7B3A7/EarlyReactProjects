5c405594ab621825fa58d55fb546032f407aefce
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2020-12-13 11:03:46 +0100

    :rocket: Initial commit

---
+++ 10_lorem_ipsum/src/App.js
@@ -0,0 +1,44 @@
+
+import { useState } from 'react';
+import data from './data';
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [text, setText] = useState([])
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count)
+    if(count <= 0){
+      amount = 1
+      setCount(amount)
+    }
+    if(amount > data.length){
+      amount = data.length
+      setCount(amount)
+    }
+    setText(data.slice(0,amount))
+  }
+
+  return (
+    <section className="section-center">
+      <h3>Tired of Lorem Ipsum?</h3>
+      <form onSubmit={handleSubmit} className="lorem-form">
+        <label htmlFor="amount">
+          paragraphs:
+        </label>
+        <input type="number" name='amount' id='amount' value={count} onChange={(e) => setCount(e.target.value)} />
+        <button type='submit' className='btn'>Generate</button>
+      </form>
+      <article className='lorem-text' >
+        {text.map((item, index) => {
+          return (
+            <p key={index}>{item}</p>
+          )
+        })}
+      </article>
+    </section>
+  );
+}
+
+export default App;
